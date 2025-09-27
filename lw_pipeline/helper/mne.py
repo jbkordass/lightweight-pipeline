eb4c5f7c7947464f7eb4cc61b81d0aa312d5a001
J.-B. Kordaß <jb.kordass@gmail.com>
2025-09-27 22:20:52 +0200

    Convenience functions for working with mne bids data added to helper.
    - find channel names
    - find associated derivative from description
    - add prefix to annotations specified via regex

--- lw_pipeline/helper/mne.py
+++ lw_pipeline/helper/mne.py
@@ -3,9 +3,10 @@
 # Authors: The Lightweight Pipeline developers
 # SPDX-License-Identifier: BSD-3-Clause
 
-import mne
-from mne_bids import BIDSPath, read_raw_bids
 import warnings
+
+import mne
+from mne_bids import BIDSPath, find_matching_paths, read_raw_bids
 
 
 def raw_from_source(source, n_jobs = 1, suppress_runtime_warning=True, **kwargs):
@@ -88,3 +89,199 @@ def raw_from_source(source, n_jobs = 1, suppress_runtime_warning=True, **kwargs)
         raise ValueError(f"Unknown source type {type(source)}.")
 
     return raw
+
+
+def find_ch(identifier, ch_names, return_identifiers=False, error_level="ignore"):
+    """
+    Find actual channel name by identifier (e.g. Fp1 for "EEG Fp1").
+
+    Parameters
+    ----------
+    identifier : str | list of str
+        The identifier to search for (e.g. "Fp1") or list of identifiers.
+    ch_names : list of str
+        The list of channel names to search in.
+    return_identifiers : bool
+        Whether to return the identifiers of found channels. Only used if identifier
+        is a list.
+    error_level : str
+        The error level to use if the channel is not found. Can be "ignore", "warn",
+        or "raise".
+
+    Returns
+    -------
+    ch_name : str | None
+        The actual channel name if found, else None.
+    """
+    if type(identifier) is list:
+        return find_chs(identifier, ch_names, return_identifiers, error_level)
+    elif not isinstance(identifier, str):
+        raise ValueError("Identifier must be a string or list of strings.")
+
+    for ch in ch_names:
+        if identifier.lower() in ch.lower():
+            return ch
+
+    if error_level == "warn":
+        warnings.warn(f"Channel {identifier} not found in {ch_names}.")
+    elif error_level == "raise":
+        raise ValueError(f"Channel {identifier} not found in {ch_names}.")
+
+    return None
+
+def find_chs(identifiers, ch_names, return_identifiers=False, error_level="ignore"):
+    """
+    Find actual channel names by identifiers (e.g. Fp1 for "EEG Fp1").
+
+    Parameters
+    ----------
+    identifiers : list of str
+        The identifiers to search for (e.g. ["Fp1", "Fp2"]).
+    ch_names : list of str
+        The list of channel names to search in.
+    return_identifiers : bool
+        Whether to return the identifiers of found channels.
+    error_level : str
+        The error level to use if a channel is not found. Can be "ignore", "warn",
+        or "raise".
+
+    Returns
+    -------
+    ch_names : list of str | None
+        The actual channel names if found, else None.
+    """
+    found_identifiers = []
+    found_ch_names = []
+    for identifier in identifiers:
+        ch_name = find_ch(identifier, ch_names, error_level=error_level)
+        if ch_name is not None:
+            found_ch_names.append(ch_name)
+            found_identifiers.append(identifier)
+        elif error_level == "raise":
+            raise ValueError(f"Channel {identifier} not found in {ch_names}.")
+
+    if return_identifiers:
+        return found_ch_names, found_identifiers
+    return found_ch_names
+
+
+def find_assoc_deriv_bidspath(
+    bids_path, description, suffix=None, extension=None, alt_description=None
+):
+    """
+    Get bids path for derivative file based on an associated bids path and description.
+
+    Use find_matching_paths based on subject, session, task, run and specify separately
+    description, suffix and extension.
+
+    Parameters
+    ----------
+    bids_path : mne_bids.BIDSPath
+        BIDSPath object with the path to the raw data file.
+    description : str
+        Description of the derivative file.
+    suffix : str
+        Suffix of the derivative file.
+    extension : str
+        Extension of the derivative file.
+    alt_description : str
+        If the first one is not found, allow to specify an alternative.
+
+    Returns
+    -------
+    bids_path : mne_bids.BIDSPath
+        BIDSPath object with the path to the derivative file.
+    """
+    if suffix is None:
+        suffix = bids_path.suffix
+    if extension is None:
+        extension = bids_path.extension
+
+    derivative_bids_path = find_matching_paths(
+        subjects=bids_path.subject,
+        sessions=bids_path.session,
+        tasks=bids_path.task,
+        runs=bids_path.run,
+        descriptions=description,
+        suffixes=suffix,
+        extensions=extension,
+        root=bids_path.root,
+    )
+
+    if len(derivative_bids_path) == 1:
+        return derivative_bids_path[0]
+    else:
+        if alt_description:
+            return find_assoc_deriv_bidspath(bids_path, alt_description, suffix, extension)
+        raise ValueError(
+            f"No derivative file found for desc {description} and subject "
+            f"{bids_path.subject} session {bids_path.session} task "
+            f"{bids_path.task} run {bids_path.run}"
+        )
+
+
+# def save_assoc_deriv(
+#     bids_path, description, suffix="eeg", extension=".fif", overwrite=False, **kwargs
+# ):
+#     """
+#     Save a derivative file associated with a BIDSPath.
+
+#     Parameters
+#     ----------
+#     bids_path : mne_bids.BIDSPath
+#         BIDSPath object with the path to the raw data file.
+#     description : str
+#         Description of the derivative file.
+#     suffix : str
+#         Suffix of the derivative file.
+#     extension : str
+#         Extension of the derivative file.
+#     overwrite : bool
+#         Whether to overwrite existing files.
+#     kwargs : dict
+#         Additional keyword arguments to pass to bids_path.copy().
+
+#     Returns
+#     -------
+#     deriv_bids_path : mne_bids.BIDSPath
+#         BIDSPath object with the path to the saved derivative file.
+#     """
+#     deriv_bids_path = bids_path.copy(
+#         suffix=suffix,
+#         description=description,
+#         extension=extension,
+#         **kwargs,
+#     )
+#     if 
+#     return deriv_bids_path
+
+
+def add_annotation_prefix(
+    annotations, prefix="BAD ", regex="(Active Stimulation)|(Buffer Stimulation)"
+):
+    """
+    Add a prefix to annotation descriptions with descriptions matching a regex.
+
+    Operates in-place.
+
+    Parameters
+    ----------
+    annotations : mne.Annotations
+        The annotations object containing the descriptions to modify.
+    prefix : str
+        The prefix to add to the annotation descriptions.
+    regex : str
+        The regex to match the annotation descriptions.
+
+    Returns
+    -------
+    annotations : mne.Annotations
+        The annotations object with updated descriptions.
+    """
+    from re import match
+
+    for i, annot in enumerate(annotations):
+        if match(regex, annot["description"]):
+            annotations.description[i] = prefix + annotations.description[i]
+
+    return annotations
